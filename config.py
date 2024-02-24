da593a9b1190c839d28e63ace9f474e756a0445b
trungnb160186 <trungnbbkno1@gmail.com>
2024-02-24 21:53:46 +0700

    Change client secret

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "2698ae8b-b4cb-43d1-987a-8e26f2db6b6b"
+    CLIENT_SECRET = "PvS8Q~ORPReoBUmcvLKDMMnqwTHnOTDYbDDeabf7"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
