91b6b67fc6b8acf322e613c0bfe5ffd2087c8e6a
trungnb160186 <trungnbbkno1@gmail.com>
2024-02-25 07:51:23 +0700

    log user

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,8 +70,9 @@ def login():
             # Log Invalid user
             app.logger.error("Invalid login attempt")
             return redirect(url_for('login'))
-        if login_user(user, remember=form.remember_me.data):
-            app.logger.warning("user logged in successfully")
+        else:
+            app.logger.info('%s logged in successfully', user.username)
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
